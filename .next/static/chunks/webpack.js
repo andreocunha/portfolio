f28edea1ad87e1e0bab8c8def708a384c224393d
andreocunha <andreoliveiracunha20@gmail.com>
2021-02-01 13:49:00 -0300

    github api

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cf0137724d7ee9a16e8f";
+/******/ 	var hotCurrentHash = "66d9ca70ce277993f90a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/pages/tempo.js
+++ src/pages/tempo.js
@@ -1,3 +1,6 @@
+// import React, { useEffect } from 'react';
+import api from '../services/api';
+
 function Tempo(props){
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
@@ -6,6 +9,10 @@ function Tempo(props){
         <div>
             <div>{dynamicDateString} (dinâmico)</div>
             <div>{props.staticDateString} (estático)</div>
+            <div>{props.dados.name} (dados)</div>
+            <div>
+                <img alt="imagem perfil" src={props.dados.avatar_url}/>
+            </div>
         </div>
     )
 }
@@ -13,10 +20,18 @@ function Tempo(props){
 export async function getStaticProps(){
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
+    let dados = {}
+
+    await api.get('/users/andreocunha').then(response => {
+        // console.log(response.data)
+        dados = response.data
+    });
+    
 
     return{
         props: {
-            staticDateString
+            staticDateString,
+            dados
         },
         revalidate: 1
     }
